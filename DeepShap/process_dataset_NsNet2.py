1255a9714b60b72b8b547002152cba7ef4ecde1f
Ivain <ivain.guittard@polytechnique.edu>
2025-05-21 20:54:14 +0200

    Add attribution computation for NsNet2

--- DeepShap/process_dataset_NsNet2.py
+++ DeepShap/process_dataset_NsNet2.py
@@ -1,3 +1,7 @@
+"""
+process_dataset.py loads noisy inputs and computes the DeepLIFTShap attributions for each frequency band. It saves the attribution map to a JSON file.
+"""
+
 import os
 import torch
 import numpy as np
@@ -7,16 +11,31 @@ from tqdm import tqdm
 
 from utils.data_utils import load_and_resample, batchify_targets, save_attributions_to_json, is_already_processed, save_path
 from models.band_feature_model import BandFeatureModel
-from config.bands import bands, sample_rate
+from DeepShap.config.parameters import bands, sample_rate
+from models.NSNet2 import NsNet2
 
-"""
-process_dataset.py loads noisy inputs"""
 # Directory containing the noisy input files
-data_dir = "/home/azureuser/cloudfiles/code/Users/iguittard/XAI-Internship/data/clean_trainset_28spk_wav"
+data_dir = "/home/azureuser/cloudfiles/code/Users/iguittard/XAI-Internship/data/noisy_trainset_28spk_wav"
+save_path = "DeepShap/attributions/deepshap_attributions_noisy_NsNet2.json"
 
 # Load the model and the DeepLIFTShap wrapper
-model = DCCRNet.from_pretrained("JorisCos/DCCRNet_Libri1Mix_enhsingle_16k")
-model.eval()
+model = NsNet2(
+    n_fft=512,
+    n_features=257,
+    hidden_1=400,
+    hidden_2=400,
+    hidden_3=600
+)
+
+# Load the pre-trained weights
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
+
+weights_path = "DeepShap/models/nsnet2_baseline.bin"
+state_dict = torch.load(weights_path, map_location=torch.device('cpu'))
+model.load_state_dict(state_dict)
+model.to(device)
+model.eval() 
 wrapped_model = BandFeatureModel(model)
 deep_shap = DeepLiftShap(wrapped_model)
 
@@ -32,7 +51,7 @@ for fname in tqdm(all_files, desc="Processing files"):
         continue
     try:
         noisy_input, sr = load_and_resample(input_path, sample_rate)
-        noisy_input = noisy_input.requires_grad_(True)
+        noisy_input = noisy_input.to(device).requires_grad_(True)
 
         # Baseline
         N = 10
@@ -44,16 +63,19 @@ for fname in tqdm(all_files, desc="Processing files"):
         attr_map = np.zeros(nb_bands)
         all_targets = list(range(nb_bands))
 
-        for batch in batchify_targets(all_targets, batch_size=2):
+        for batch in batchify_targets(all_targets, batch_size=64):
             print(f"Computing attribution for file {fname}, batch {batch}")
             input_batch = noisy_input.repeat(len(batch), 1, 1)
-            attrs = deep_shap.attribute(input_batch, baselines=baseline, target=batch)
+            with torch.set_grad_enabled(True):
+                model.train()  # Temporarily set the model to training mode
+                attrs = deep_shap.attribute(input_batch, baselines=baseline, target=batch)
+                model.eval()  # Reset the model back to evaluation mode
             attrs = attrs.squeeze().cpu().detach().numpy()
             for idx, b in enumerate(batch):
                 attr_map[b] = attrs[idx].mean()
 
         # Save the attribution map to a JSON file
-        save_attributions_to_json(attr_map, bands, input_path)
+        save_attributions_to_json(attr_map, bands, input_path, save_path)
 
     except Exception as e:
         print(f"[ERROR] Failed processing {fname}: {e}")
